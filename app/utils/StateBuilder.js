a2385811b29dcc28613a8d7009e57f9cfd95987d
Lex-Ai <12001684+Lex-Ai@users.noreply.github.com>
2020-07-04 19:02:06 +0300

    fix mobile menu

--- app/utils/StateBuilder.js
+++ app/utils/StateBuilder.js
@@ -54,7 +54,7 @@ export default async function getState(api, url, options, offchain = {}) {
 
             switch (parts[1]) {
                 case 'transfers':
-                    const history = await api.getAccountHistory(uname, -1, 1000, ['producer_reward','fill_vesting_withdraw'])
+                    const history = await api.getAccountHistory(uname, -1, 1000, ['producer_reward'])
                     account.transfer_history = []
                     account.other_history = []
 
@@ -205,7 +205,7 @@ export default async function getState(api, url, options, offchain = {}) {
         state.cprops = await api.getChainProperties();
   
     } else if (Object.keys(PUBLIC_API).includes(parts[0])) {
-        let args = { limit: 20, truncate_body: 1024, period_sec: 604800 }
+        let args = { limit: 20, truncate_body: 1024 }
         const discussionsType = parts[0]
         if (typeof tag === 'string' && tag.length && (!tag.startsWith('tag-') || tag.length > 4)) {
             if (tag.startsWith('tag-')) {
