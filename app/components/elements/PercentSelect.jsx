7027548c55aa289f78921955898f13368dbea1c2
isteric-senior <halfyearforwhat@gmail.com>
2020-07-21 08:53:07 +0300

    Fix worker routes

--- app/components/elements/PercentSelect.jsx
+++ app/components/elements/PercentSelect.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class PercentSelect extends React.Component {
   render() {
     let options = [];
-    for (var i = 10; i <= 100; i+=10) {
+    for (var i = 100; i >= 10; i-=10) {
       options.push(<option key={i*100} value={i*100}>{i}%</option>);
     }
     return(
