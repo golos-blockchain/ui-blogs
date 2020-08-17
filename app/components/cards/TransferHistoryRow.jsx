da26a1e2180e44698f752cf0cb32caf51431a1aa
Lex-Ai <12001684+Lex-Ai@users.noreply.github.com>
2020-08-17 20:21:05 +0300

    minor fix

--- app/components/cards/TransferHistoryRow.jsx
+++ app/components/cards/TransferHistoryRow.jsx
@@ -6,15 +6,12 @@ import Tooltip from 'app/components/elements/Tooltip';
 import Memo from 'app/components/elements/Memo'
 import {numberWithCommas, vestsToSp} from 'app/utils/StateFunctions'
 import tt from 'counterpart';
-import { VEST_TICKER, LIQUID_TICKER, DEBT_TOKEN_SHORT } from 'app/client_config';
+import { VEST_TICKER } from 'app/client_config';
 import {PrivateKey} from 'golos-classic-js/lib/auth/ecc';
 
 class TransferHistoryRow extends React.Component {
 
     render() {
-        const LIQUID_TOKEN = tt('token_names.LIQUID_TOKEN')
-        const DEBT_TOKEN = tt('token_names.DEBT_TOKEN')
-        const DEBT_TOKENS = tt('token_names.DEBT_TOKENS')
         const VESTING_TOKEN =  tt('token_names.VESTING_TOKEN')
         const VESTING_TOKENS = tt('token_names.VESTING_TOKENS')
 
@@ -26,6 +23,7 @@ class TransferHistoryRow extends React.Component {
 
         /*  all transfers involve up to 2 accounts, context and 1 other. */
         let description_start = ""
+        let code_key = null;
         let other_account = null;
         let description_end = "";
         let data_memo = data.memo;
@@ -34,10 +32,10 @@ class TransferHistoryRow extends React.Component {
             const amount = data.amount && data.amount.split && data.amount.split(' ')[0]
             if( data.from === context ) {
                 if( data.to === "" ) {
-                    description_start += tt('g.transfer') + data.amount.split(' ')[0] + tt('g.to') + " " + VESTING_TOKEN;
+                    description_start += tt('transferhistoryrow_jsx.transferred') + data.amount.split(' ')[0] + tt('g.to') + " " + VESTING_TOKEN;
                 }
                 else {
-                    description_start += tt('g.transfer') + data.amount.split(' ')[0] + " " + VESTING_TOKEN + tt('g.to');
+                    description_start += tt('transferhistoryrow_jsx.transferred') + data.amount.split(' ')[0] + " " + VESTING_TOKEN + tt('g.to');
                     other_account = data.to;
                 }
             }
@@ -45,12 +43,12 @@ class TransferHistoryRow extends React.Component {
                 description_start += tt('g.receive') + " " + data.amount.split(' ')[0] + " " + VESTING_TOKEN + tt('g.from');
                 other_account = data.from;
             } else {
-                description_start += tt('g.transfer') + data.amount.split(' ')[0] + " " + VESTING_TOKEN + tt('g.from') + data.from + tt('g.to');
+                description_start += tt('transferhistoryrow_jsx.transferred') + data.amount.split(' ')[0] + " " + VESTING_TOKEN + tt('g.from') + data.from + tt('g.to');
                 other_account = data.to;
             }
         }
+
         else if(/^transfer$|^transfer_to_savings$|^transfer_from_savings$/.test(type)) {
-            // transfer_to_savings
             const fromWhere =
                 type === 'transfer_to_savings' ? tt('transferhistoryrow_jsx.to_savings') :
                 type === 'transfer_from_savings' ? tt('transferhistoryrow_jsx.from_savings') :
@@ -64,36 +62,43 @@ class TransferHistoryRow extends React.Component {
                 description_start += tt('g.receive') + ` ${fromWhere} ${data.amount}` + tt('g.from');
                 other_account = data.from;
             } else {
-                description_start += tt('g.transfer') + ` ${fromWhere} ${data.amount}` + tt('g.from');
+                description_start += tt('transferhistoryrow_jsx.transferred') + ` ${fromWhere} ${data.amount}` + tt('g.from');
                 other_account = data.from;
                 description_end += tt('g.to') + data.to;
             }
             if(data.request_id != null)
                 description_end += ` (${tt('g.request')} ${data.request_id})`
         }
+
         else if (type === 'cancel_transfer_from_savings') {
             description_start += `${tt('transferhistoryrow_jsx.cancel_transfer_from_savings')} (${tt('g.request')} ${data.request_id})`;
         }
+
         else if( type === 'withdraw_vesting' ) {
             if( data.vesting_shares === '0.000000 ' + VEST_TICKER)
                 description_start += tt('transferhistoryrow_jsx.stop_power_down', {VESTING_TOKENS});
             else
                 description_start += tt('transferhistoryrow_jsx.start_power_down_of', {VESTING_TOKENS}) + ' ' +  data.vesting_shares;
         }
+
         else if( type === 'curation_reward' ) {
             description_start += `${curation_reward} ${VESTING_TOKENS}` + tt('transferhistoryrow_jsx.for');
             other_account = data.comment_author + "/" + data.comment_permlink;
         }
+
         else if (type === 'author_reward') {
             description_start += `${author_reward} ${VESTING_TOKENS}` + tt('transferhistoryrow_jsx.for');
             other_account = data.author + "/" + data.permlink;
         }
+
         else if (type === 'interest') {
             description_start += `${tt('transferhistoryrow_jsx.receive_interest_of')} ${data.interest}`;
         }
+
         else if (type === 'fill_convert_request') {
             description_start += `Fill convert request: ${data.amount_in} for ${data.amount_out}`;
         }
+
         else if (type === 'fill_order') {
             if(data.open_owner == context) {
                 // my order was filled by data.current_owner
@@ -103,6 +108,7 @@ class TransferHistoryRow extends React.Component {
                 description_start += `Paid ${data.current_pays} for ${data.open_pays}`;
             }
         }
+
         else if (type === 'donate' && context == 'ref') {
             const donate_meta = JSON.parse(op[1].json_metadata);
             description_start += donate_meta.referrer_interest + tt('transferhistoryrow_jsx.percen_referral');
@@ -149,6 +155,18 @@ class TransferHistoryRow extends React.Component {
                 data_memo = '';
             }
         }
+
+        else if (type === 'invite') {
+            description_start += tt('invites_jsx.hist_invite');
+            code_key = data.invite_key;
+            description_end += tt('invites_jsx.hist_invite2') + data.balance;            
+        }
+
+        else if (type === 'invite_claim') {
+            description_start += tt('invites_jsx.hist_claimed');
+            code_key = PrivateKey.fromWif(data.invite_secret).toPublicKey().toString();
+        }
+
         else if (type === 'claim') {
             if( data.to === context ) {
                 description_start += tt('g.receive') + " " + data.amount + tt('transferhistoryrow_jsx.with_claim') + tt('transferhistoryrow_jsx.from');
@@ -161,13 +179,7 @@ class TransferHistoryRow extends React.Component {
                 description_end += tt('transferhistoryrow_jsx.to_golos_power');
             }         
         }
-        else if (type === 'invite') {
-            description_start += tt('invites_jsx.hist_invite') + data.invite_key + tt('invites_jsx.hist_invite2') + data.balance;
-        }
-        else if (type === 'invite_claim') {
-            description_start += tt('invites_jsx.hist_claimed');
-            description_start += PrivateKey.fromWif(data.invite_secret).toPublicKey().toString();
-        }
+
         else if (type === 'transfer_to_tip') {
             if( data.to === context ) {
                 description_start += tt('g.receive') + " " + data.amount + tt('transferhistoryrow_jsx.to_tip') + tt('transferhistoryrow_jsx.from');
@@ -177,6 +189,7 @@ class TransferHistoryRow extends React.Component {
                 other_account = data.to;
             }
         }
+
         else if (type === 'transfer_from_tip') {
             if( data.to === context ) {
                 description_start += tt('g.receive') + " " + data.amount + tt('transferhistoryrow_jsx.from_tip') + tt('transferhistoryrow_jsx.from');
@@ -188,6 +201,7 @@ class TransferHistoryRow extends React.Component {
                 description_end += tt('transferhistoryrow_jsx.to_golos_power');
             }
         }
+
         else if (type === 'worker_reward') {
             description_start += tt('transferhistoryrow_jsx.funded_workers') + data.reward + tt('transferhistoryrow_jsx.for');
             other_account = data.worker_request_author + "/" + data.worker_request_permlink;
@@ -195,6 +209,7 @@ class TransferHistoryRow extends React.Component {
         else {
             description_start += JSON.stringify({type, ...data}, null, 2);
         }
+
         return(
                 <tr key={op[0]} className="Trans">
                     <td>
@@ -204,6 +219,7 @@ class TransferHistoryRow extends React.Component {
                     </td>
                     <td className="TransferHistoryRow__text" style={{maxWidth: "40rem"}}>
                         {description_start}
+                        {code_key && <span style={{ fontSize: '85%' }}>{code_key}</span>}
                         {other_account && <Link to={`/@${other_account}`}>{other_account}</Link>}
                         {description_end}
                     </td>
@@ -214,8 +230,6 @@ class TransferHistoryRow extends React.Component {
         );
     }
 }
-
-const renameToSd = txt => txt ? numberWithCommas(txt.replace('SBD', DEBT_TOKEN_SHORT)) : txt
 
 export default connect(
     // mapStateToProps
