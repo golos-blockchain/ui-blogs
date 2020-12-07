9619f21f465204adf9a9c0f012e4ae4978325a07
Lex-Ai <12001684+Lex-Ai@users.noreply.github.com>
2020-12-07 20:12:43 +0300

    minor fix

--- app/client_config.js
+++ app/client_config.js
@@ -68,7 +68,7 @@ export const PUBLIC_API = {
   author:     'getDiscussionsByBlogAsync',
 }
 
-export const SEO_TITLE = 'Блоги на блокчейне Golos'
+export const SEO_TITLE = 'GOLOS.id Блоги'
 export const USER_GENDER = ['undefined', 'male', 'female']
 
 export const CHANGE_IMAGE_PROXY_TO_STEEMIT_TIME = 1568627859000
