095fe3d120d9dd99a85d71bc3e837921cd7d7bcf
1aerostorm <aerostorm1@gmail.com>
2021-06-01 00:07:24 +0000

    Update sequelize, mysql -> mysql2

--- db/migrations/20160519211043-users-waiting-list.js
+++ db/migrations/20160519211043-users-waiting-list.js
@@ -2,12 +2,16 @@
 
 module.exports = {
     up: function (queryInterface, Sequelize) {
-        queryInterface.addColumn('users', 'waiting_list', Sequelize.BOOLEAN);
-        queryInterface.addColumn('users', 'remote_ip', Sequelize.STRING);
+        return Promise.all([
+            queryInterface.addColumn('users', 'waiting_list', Sequelize.BOOLEAN),
+            queryInterface.addColumn('users', 'remote_ip', Sequelize.STRING),
+        ]);
     },
 
     down: function (queryInterface, Sequelize) {
-        queryInterface.removeColumn('users', 'waiting_list');
-        queryInterface.removeColumn('users', 'remote_ip');
+        return Promise.all([
+            queryInterface.removeColumn('users', 'waiting_list'),
+            queryInterface.removeColumn('users', 'remote_ip'),
+        ]);
     }
 };
