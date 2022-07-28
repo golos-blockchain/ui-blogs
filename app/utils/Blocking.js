7378df5288f9cdcc4229219fba4c70f1c20ab0d8
1aerostorm <aerostorm1@gmail.com>
2022-07-28 20:11:42 +0000

    HF27 Do not bother fix

--- app/utils/Blocking.js
+++ app/utils/Blocking.js
@@ -2,35 +2,42 @@ import { api } from 'golos-lib-js'
 import { Asset } from 'golos-lib-js/lib/utils'
 import tt from 'counterpart'
 
-export async function checkBlocking(blocker, blocking, tipAmount = false) {
-    const rels = await api.getAccountRelationsAsync({
-        my_account: blocker,
-        with_accounts: [blocking]
-    })
-    if (rels[blocking] && rels[blocking].blocking) {
-        const cprops = await api.getChainPropertiesAsync()
-        if (cprops) {
-            let acc = await api.getAccountsAsync([blocking])
-            acc = acc[0]
-            const tipBalance = Asset(acc.tip_balance)
-            let cost = Asset(cprops.unwanted_operation_cost)
-            if (tipAmount && tipAmount.symbol === 'GOLOS') {
-                cost = cost.plus(tipAmount)
-            }
+async function checkBalance(blocking, tipAmount, isBother = false) {
+    const cprops = await api.getChainPropertiesAsync()
+    const tipBalance = Asset(blocking.tip_balance)
+    let cost = Asset(cprops.unwanted_operation_cost)
+    if (tipAmount && tipAmount.symbol === 'GOLOS') {
+        cost = cost.plus(tipAmount)
+    }
 
-            if (cost.gt(tipBalance)) {
-                return { error: tt('do_not_bother.blocking_error', {
-                        AMOUNT: cost.toString()
-                    })
-                }
-            }
+    const key = isBother ? 'bother' : 'blocking'
 
-            return {
-                confirm: tt('do_not_bother.blocking_confirm', {
-                        AMOUNT: cost.toString()
-                    })
-            }
+    if (cost.gt(tipBalance)) {
+        return { error: tt(`do_not_bother.${key}_error`, {
+                AMOUNT: cost.toString()
+            })
         }
+    }
+
+    return {
+        confirm: tt(`do_not_bother.${key}_confirm`, {
+                AMOUNT: cost.toString()
+            })
+    }
+}
+
+export async function checkBlocking(blockerName, blockingName, tipAmount = false) {
+    const accs = await api.getAccountsAsync([blockerName, blockingName])
+    if (!accs || accs.length !== 2) return {}
+    const [ blocker, blocking ] = accs
+    const rels = await api.getAccountRelationsAsync({
+        my_account: blockerName,
+        with_accounts: [blockingName]
+    })
+    if (rels[blockingName] && rels[blockingName].blocking) {
+        return await checkBalance(blocking, tipAmount)
+    } else if (blocker.do_not_bother && blocking.reputation < 27800000000000) {
+        return await checkBalance(blocking, tipAmount, true)
     }
     return {}
 }
