4fa600cee8aa413e0414938efd6d30216461065d
1aerostorm <aerostorm1@gmail.com>
2022-09-24 08:50:46 +0000

    Support app:// in LinkEx

--- app/utils/LinkEx.jsx
+++ app/utils/LinkEx.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 
 const isExternal = (url) => {
-	return /^https?:\/\//.test(url)
+	return /^(.*?):\/\//.test(url)
 }
 
 class LinkEx extends React.Component {
