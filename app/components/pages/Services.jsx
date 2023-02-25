abc3b4d808af482dc34883477b2e0cb1bf29b1e8
Lex-Ai <12001684+Lex-Ai@users.noreply.github.com>
2023-02-25 22:21:31 +0300

    minor fix

--- app/components/pages/Services.jsx
+++ app/components/pages/Services.jsx
@@ -23,7 +23,7 @@ class Services extends React.Component {
                             <div className='column large-3 medium-3 small-6'>
                                 <a target="_blank" href="https://wallet.golos.id"><Icon name='money' size='10x'/>
                                 <h5>Golos Wallet + DEX</h5></a>
-                                <p className='landing-services-block-icon-text'>Кошелек и торговля на <a target="_blank" href="https://wallet.golos.id/market">внутренней бирже</a> и <a target="_blank" href="https://wallet.golos.id/convert">обменнике</a>, веб-клиентах <a target="_blank" href="https://dex.golos.app/#/trade/GOLOS_YMUSDT">dex.golos.app</a> или <a target="_blank" href="https://gls.exchange">gls.exchange</a></p>
+                                <p className='landing-services-block-icon-text'>Кошелек и торговля на <a target="_blank" href="https://wallet.golos.id/market/GOLOS/YMUSDT">внутренней бирже</a> и <a target="_blank" href="https://wallet.golos.id/convert/GOLOS/YMUSDT">обменнике</a>, веб-клиентах <a target="_blank" href="https://dex.golos.app/#/trade/GOLOS_YMUSDT">dex.golos.app</a> или <a target="_blank" href="https://gls.exchange/#/trade/GOLOS_YMUSDT">gls.exchange</a></p>
                             </div>
                             <div className='column large-3 medium-3 small-6'>
                                 <a target="_blank" href="https://chat.golos.app"><Icon name='rocket' size='10x'/>
