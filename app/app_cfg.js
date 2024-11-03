b5f9eb0ee016025d6ca5080190c0b6bda917c3e1
1aerostorm <aerostorm1@gmail.com>
2024-11-04 01:44:51 +0300

    Mobile app - Fix

---
+++ app/app_cfg.js
@@ -0,0 +1,79 @@
+/* Only Mobile. Generated automatically. Do not edit. */
+module.exports = {
+    "app_version": "1.5.0",
+    "ws_connection_app": [
+        {
+            "address": "wss://apibeta.golos.today/ws"
+        },
+        {
+            "address": "wss://api.golos.id/ws"
+        },
+        {
+            "address": "wss://api.aleksw.space/ws"
+        },
+        {
+            "address": "wss://api-golos.blckchnd.com/ws"
+        }
+    ],
+    "ws_connection_exchange": "wss://apibeta.golos.today/ws",
+    "logo": {
+        "icon": "https://i.imgur.com/Q7GCdPf.png",
+        "title": "https://i.imgur.com/36zv8We.png"
+    },
+    "images": {
+        "img_proxy_prefix": "https://devimages.golos.today",
+        "img_proxy_backup_prefix": "https://steemitimages.com",
+        "upload_image": "https://api.imgur.com/3/image",
+        "client_id": "6c09ebf8c548126"
+    },
+    "wallet_service": {
+        "host": "https://devwallet.golos.today"
+    },
+    "messenger_service": {
+        "host": "https://devchat.golos.app"
+    },
+    "auth_service": {
+        "host": "https://dev.golos.app",
+        "custom_client": "blogs"
+    },
+    "notify_service": {
+        "host": "https://devnotify.golos.app",
+        "host_ws": "wss://devnotify.golos.app/ws"
+    },
+    "elastic_search": {
+        "url": "https://search.golos.today",
+        "login": "golosclient",
+        "password": "golosclient"
+    },
+    "apidex_service": {
+        "host": "https://devapi-dex.golos.app",
+        "host_local": "https://devapi-dex.golos.app"
+    },
+    "app_updater": {
+        "host": "https://files.golos.app"
+    },
+    "forums": {
+        "white_list": [
+            "fm-golostalk",
+            "fm-prizmtalk",
+            "fm-graphenetalks"
+        ],
+        "fm-golostalk": {
+            "domain": "golostalk.com"
+        },
+        "fm-prizmtalk": {
+            "domain": "prizmtalk.com"
+        },
+        "fm-graphenetalks": {
+            "domain": "forum.gph.ai"
+        }
+    },
+    "hidden_assets": {
+        "RUDEX": true,
+        "PRIZM": true,
+        "DOGECOIN": true,
+        "YMZEC": true,
+        "YMWMZ": true,
+        "YMBTC": true
+    }
+}
