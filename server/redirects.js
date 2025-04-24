d947b57911327fae4420e307dbe5d85c0d0aedc0
1aerostorm <aerostorm1@gmail.com>
2025-04-24 21:52:34 +0300

    Update koa and related packages

--- server/redirects.js
+++ server/redirects.js
@@ -1,4 +1,4 @@
-import koa_router from 'koa-router';
+import Router from 'koa-router'
 
 const redirects = [
     // example: [/\/about(\d+)-(.+)/, '/about?$0:$1', 302],
@@ -6,16 +6,16 @@ const redirects = [
 ];
 
 export default function useRedirects(app) {
-    const router = koa_router();
+    const router = new Router()
 
     app.use(router.routes());
 
     redirects.forEach(r => {
-        router.get(r[0], function *() {
-            const dest = Object.keys(this.params).reduce((value, key) => value.replace('$' + key, this.params[key]), r[1]);
-            console.log(`server redirect: [${r[0]}] ${this.request.url} -> ${dest}`);
-            this.status = r[2] || 301;
-            this.redirect(dest);
+        router.get(r[0], (ctx) => {
+            const dest = Object.keys(ctx.params).reduce((value, key) => value.replace('$' + key, ctx.params[key]), r[1]);
+            console.log(`server redirect: [${r[0]}] ${ctx.request.url} -> ${dest}`);
+            ctx.status = r[2] || 301;
+            ctx.redirect(dest);
         });
     });
 }
