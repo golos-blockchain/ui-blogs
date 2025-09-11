de35a24a824fd4c64c89eadab2b24e2e0ccb77bb
1aerostorm <aerostorm1@gmail.com>
2025-09-11 04:15:24 +0300

    universal webpack, #12. Fix process.env in server build.

--- webpack/uni/dev.config.server.babel.js
+++ webpack/uni/dev.config.server.babel.js
@@ -1,5 +1,12 @@
 import { server } from 'universal-webpack/config'
+import webpack from 'webpack';
+
 import settings from './universal-webpack-settings'
+import { fixConfigForServer } from './utils'
 import configuration from '../dev.config'
 
-export default server(configuration, settings)
+const cfg = server(configuration, settings)
+
+fixConfigForServer(cfg);
+
+export default cfg;
--- webpack/uni/prod.config.server.babel.js
+++ webpack/uni/prod.config.server.babel.js
@@ -1,5 +1,10 @@
 import { server } from 'universal-webpack/config'
 import settings from './universal-webpack-settings'
+import { fixConfigForServer } from './utils'
 import configuration from '../prod.config'
 
-export default server(configuration, settings)
+const cfg = server(configuration, settings)
+
+fixConfigForServer(cfg);
+
+export default cfg;
