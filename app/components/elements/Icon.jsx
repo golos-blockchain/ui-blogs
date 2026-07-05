d24614f89e40e57d88a8b08aa16d55ea48ff3901
1aerostorm <aerostorm1@gmail.com>
2026-07-05 07:40:01 +0300

    Update+unify UI lib

--- app/components/elements/Icon.jsx
+++ app/components/elements/Icon.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const icons = new Map([
     ['user', require('app/assets/icons/user.svg')],
@@ -146,7 +146,7 @@ const icons = new Map([
     ['notification/mention', require('app/assets/icons/notification/mention.svg')],
     ['notification/message', require('app/assets/icons/notification/message.svg')],
     ['notification/order', require('app/assets/icons/notification/order.svg')],
-]);
+])
 
 const rem_sizes = {
     '0_75x': '0.75',
@@ -160,58 +160,51 @@ const rem_sizes = {
     '4x': '4.60',
     '5x': '5.75',
     '10x': '10.0',
-};
-
-export default class Icon extends React.PureComponent {
-    static propTypes = {
-        name: PropTypes.string.isRequired,
-        size: PropTypes.oneOf([
-            '0_75x',
-            '0_95x',
-            '1x',
-            '1_25x',
-            '1_5x',
-            '1_75x',
-            '2x',
-            '3x',
-            '4x',
-            '5x',
-            '10x',
-        ]),
-    };
-
-    render() {
-        const { name, size, className, noNewLine } = this.props;
-        let classes = 'Icon ' + name;
-        let style;
-
-        if (size) {
-            classes += ' Icon_' + size;
-            style = { width: `${rem_sizes[size]}rem` };
-        }
-
-        if (className) {
-            classes += ' ' + className;
-        }
-
-        const passProps = { ...this.props };
-        delete passProps.name;
-        delete passProps.size;
-        delete passProps.className;
-        delete passProps.noNewLine;
-
-        let svg = icons.get(name);
-        if (noNewLine) {
-            svg = svg.replace(/\n/g, ''); // or we can use svgo-loader for such optimization
-        }
-
-        return (
-            <span
-                {...passProps}
-                className={classes}
-                style={style}
-                dangerouslySetInnerHTML={{ __html: svg }}
-            />
-        );
-    }
 }
+
+const Icon = ({ name, size, className, noNewLine, ...passProps }) => {
+    let classes = 'Icon ' + name
+    let style
+
+    if (size) {
+        classes += ' Icon_' + size
+        style = { width: `${rem_sizes[size]}rem` }
+    }
+
+    if (className) {
+        classes += ' ' + className
+    }
+
+    let svg = icons.get(name)
+    if (noNewLine) {
+        svg = svg.replace(/\n/g, '')
+    }
+
+    return (
+        <span
+            {...passProps}
+            className={classes}
+            style={style}
+            dangerouslySetInnerHTML={{ __html: svg }}
+        />
+    )
+}
+
+Icon.propTypes = {
+    name: PropTypes.string.isRequired,
+    size: PropTypes.oneOf([
+        '0_75x',
+        '0_95x',
+        '1x',
+        '1_25x',
+        '1_5x',
+        '1_75x',
+        '2x',
+        '3x',
+        '4x',
+        '5x',
+        '10x',
+    ]),
+}
+
+export default Icon
