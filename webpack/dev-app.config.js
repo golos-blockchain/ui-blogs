81e9178e2213aea31079e38fa2d443161e87764c
1aerostorm <aerostorm1@gmail.com>
2026-09-14 05:30:23 +0300

    Dart - Setup webpack config

--- webpack/dev-app.config.js
+++ webpack/dev-app.config.js
@@ -53,7 +53,15 @@ module.exports = merge(baseConfig, {
                             sourceMap: true,
                         },
                     },
-                    { loader: 'sass-loader', options: { sourceMap: true } },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            sassOptions: {
+                                quietDeps: true,
+                            },
+                        },
+                    },
                 ],
             },
         ],
